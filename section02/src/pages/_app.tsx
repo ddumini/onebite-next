132212e9297e14e7fab87ff654e3e2ec6305f28e
sumin <tnals2565@naver.com>
2025-05-27 10:12:19 +0900

    study: global layout

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,35 +1,11 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import GlobalLayout from '@/components/global-layout';
+
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  const onClickButton = () => {
-    router.push('/test');
-  };
-
-  useEffect(() => { // 명시적으로 pre-fetch
-    router.prefetch('/test');
-  }, []);
-
   return (
-    <>
-      <header>
-        <Link href='/'>홈</Link>
-        &nbsp;
-        <Link href='/search' prefetch={false}>
-        {/* 명시적으로 pre-fetch 해제 */}
-          검색
-        </Link>
-        &nbsp;
-        <Link href='/book/1'>책/1</Link>
-        <div>
-          <button onClick={onClickButton}>/test 페이지로 이동</button>
-        </div>
-      </header>
+    <GlobalLayout>
       <Component {...pageProps} />
-    </>
+    </GlobalLayout>
   );
 }
