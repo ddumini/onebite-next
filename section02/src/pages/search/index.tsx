8ee4f64b35c250c8a895e7f36076e8962a316612
sumin <tnals2565@naver.com>
2025-05-27 16:41:20 +0900

    study: onebite books ui

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,13 +1,12 @@
-import { useRouter } from "next/router";
 import { ReactNode } from "react";
 import SearchableLayout from "@/components/searchable-layout";
+import books from "@/mock/books.json";
+import BookItem from '@/components/book-item';
 
 export default function Page() {
-  const router = useRouter();
-  const { q } = router.query;
-
-  console.log(router)
-  return <h1>{q}</h1>;
+  return <div>
+    {books.map((book) => (<BookItem key={book.id} {...book} />))}
+  </div>;
 }
 Page.getLayout = (page: ReactNode) => {
   return <SearchableLayout>{page}</SearchableLayout>;
