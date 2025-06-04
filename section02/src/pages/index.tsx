85ed98ca61c4e4e773dfaf65967869aec7dcbbaa
sumin <tnals2565@naver.com>
2025-06-04 16:03:11 +0900

    ssg

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -2,11 +2,11 @@ import { ReactNode } from 'react';
 import style from './index.module.css';
 import SearchableLayout from '@/components/searchable-layout';
 import BookItem from '@/components/book-item';
-import { InferGetServerSidePropsType } from 'next';
+import { InferGetStaticPropsType } from 'next';
 import fetchBooks from '@/lib/fetch-books';
 import fetchRandomBooks from '@/lib/fetch-random-books';
 
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
   // 컴포넌트보다 먼저 실행되어서 컴포넌트에 필요한 데이터 불러오는 함수
   const [allBooks, recoBooks] = await Promise.all([
     fetchBooks(),
@@ -19,8 +19,7 @@ export const getServerSideProps = async () => {
     }
   }
 };
-export default function Home({allBooks, recoBooks} : InferGetServerSidePropsType<typeof getServerSideProps>) {
-
+export default function Home({ allBooks, recoBooks }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
       <div className={style.container}>
--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,23 +1,42 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import SearchableLayout from "@/components/searchable-layout";
 import BookItem from '@/components/book-item';
+import { useRouter } from 'next/router';
 import fetchBooks from '@/lib/fetch-books';
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+import { BookData } from '@/types';
+// import fetchBooks from '@/lib/fetch-books';
+// import { GetStaticPropsContext, InferGetStaticPropsType } from 'next';
 
-export const getServerSideProps = async (context : GetServerSidePropsContext) => {
-  const q = context.query.q;
-  const books = await fetchBooks(q as string);
-  return {
-    props: {
-      books,
-    },
-  };
-}
+// export const getStaticSideProps = async (context : GetStaticPropsContext) => {
+//   const q = context.query.q;
+//   const books = await fetchBooks(q as string);
+//   return {
+//     props: {
+//       books,
+//     },
+//   };
+// }
 
-export default function Page({books} : InferGetServerSidePropsType<typeof getServerSideProps>) {
-  return <div>
-    {books.map((book) => (<BookItem key={book.id} {...book} />))}
-  </div>;
+export default function Page() {
+  const [books, setBooks] = useState<BookData[]>([]);
+  const router = useRouter();
+  const q = router.query.q;
+  const fetchSearchResult = async() => {
+    const data = await fetchBooks(q as string);
+    setBooks(data);
+  }
+  useEffect(() => {
+    if (q) {
+      fetchSearchResult();
+    }
+  }, [q])
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
 Page.getLayout = (page: ReactNode) => {
   return <SearchableLayout>{page}</SearchableLayout>;
