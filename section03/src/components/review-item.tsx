790f96c0126dda07bdce300d6debf0a80d446c5f
sumin <tnals2565@naver.com>
2025-07-04 18:05:34 +0900

    study: delete review

--- section03/src/components/review-item.tsx
+++ section03/src/components/review-item.tsx
@@ -1,14 +1,15 @@
 import { ReviewData } from '@/types';
 import styles from './review-item.module.css';
+import ReviewItemDeleteButton from './review-item-delete-button';
 
-export default function ReviewItem({ content, author, createdAt}: ReviewData) {
+export default function ReviewItem({ id, content, author, createdAt, bookId }: ReviewData) {
   return (
     <div className={styles.container}>
       <div className={styles.author}>{author}</div>
       <div className={styles.content}>{content}</div>
       <div className={styles.bottom_container}>
         <div className={styles.date}>{new Date(createdAt).toLocaleString()}</div>
-        <div className={styles.delete_btn}>삭제하기</div>
+        <ReviewItemDeleteButton reviewId={id} bookId={bookId} />
       </div>
     </div>
   );
