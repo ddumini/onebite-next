b277f68312b82848761d01765d0165777c4859ea
sumin <tnals2565@naver.com>
2025-07-04 16:41:34 +0900

    study: variety revalidate

--- section03/src/actions/create-review.action.tsx
+++ section03/src/actions/create-review.action.tsx
@@ -1,6 +1,7 @@
 'use server';
 
-import { revalidatePath } from "next/cache";
+// import { revalidatePath, revalidateTag } from "next/cache";
+import { revalidateTag } from 'next/cache';
 
 export async function createReviewAction(formData: FormData) {
   const bookId = formData.get('bookId')?.toString();
@@ -17,7 +18,19 @@ export async function createReviewAction(formData: FormData) {
       body: JSON.stringify({ bookId, content, author }),
     });
     console.log(response);
-    revalidatePath(`/book/${bookId}`);
+    // 1. 특정 주소의 해당하는 페이지만 재검증
+    // revalidatePath(`/book/${bookId}`);
+    // 2. 특정 경로의 모든 동적 페이직를 재검증
+    // revalidatePath('/book/[id]', 'page');
+    // 3. 특정 레이아웃을 갖는 모든 페이지 재검증
+    // revalidatePath('/(with-searchbar)', 'layout');
+    // 4. 모든 페이지 재검증
+    // revalidatePath('/', 'layout');
+    // 5. 태그 기준, 데이터 캐시 재검증 (이 태그를 가진 데이터 캐시가 재검증됨) - 권장
+    // const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/book/${bookId}`, {
+    //   next: { tags: [`review-${bookId}`] },
+    // });
+    revalidateTag(`review-${bookId}`);
   } catch (err) {
     console.error(err);
     return;
--- section03/src/app/book/[id]/page.tsx
+++ section03/src/app/book/[id]/page.tsx
@@ -38,7 +38,9 @@ return (
 }
 
 async function ReviewList({bookId}: {bookId: string}) {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/book/${bookId}`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/book/${bookId}`, {
+    next: { tags: [`review-${bookId}`] },
+  });
   if (!response.ok) {
     throw new Error(`Review fetch failed: ${response.statusText}`);
   }
